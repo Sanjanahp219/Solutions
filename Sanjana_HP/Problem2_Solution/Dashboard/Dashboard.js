a5ccc6e791b677d1b767136ce1d11c6823d2bc5d
Sanjanahp219 <121929168+Sanjanahp219@users.noreply.github.com>
2024-03-20 20:53:16 +0530

    Add files via upload

---
+++ Sanjana_HP/Problem2_Solution/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+// Dashboard.js
+
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Add dashboard content here */}
+    </div>
+  );
+};
+
+export default Dashboard;
